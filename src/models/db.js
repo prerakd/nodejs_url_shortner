eae17b1cbc91e50d0ce084037e91d3e72d8f408d
Prerak Dholakiya <prerak.dholakia@gmail.com>
2021-05-28 09:49:39 +0530

    Implementing URL creation and retrieving

--- src/models/db.js
+++ src/models/db.js
@@ -10,7 +10,7 @@ const db = new Sequelize({
 const URLs = db.define('urls', {
     id: {
         primaryKey: true,
-        type: DataTypes.INTEGER
+        type: DataTypes.BIGINT
     },
     code: {
         type: DataTypes.STRING(7),
@@ -23,5 +23,6 @@ const URLs = db.define('urls', {
 })
 
 module.exports = {
-    db
+    db,
+    URLs
 }
